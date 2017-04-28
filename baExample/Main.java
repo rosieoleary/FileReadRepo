ae8d162cde5c64b98f4379330915cf7f3bddd7d8
rosieoleary <rosie651@btinternet.com>
2017-04-28 15:27:03 +0000

    first commit

---
+++ baExample/Main.java
@@ -0,0 +1,15 @@
+//package baExample;
+
+//import java.io.IOException;
+
+//public class Main {
+	
+	//public static void main(String[] args) throws IOException{
+		//readFile r= new readFile();
+		//r.bufferedcopy("ExData.txt");
+		
+		//Animal a1 =new Animal(r.personpetArray[16], r.personpetArray[17]);
+	//}
+	
+
+//}
